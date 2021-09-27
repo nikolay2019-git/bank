72d7d7ae0d33ca5bf94f1a42ad0149ea0cf69528
Timofey Savenko <tim_savenko@go.alif.academy>
2021-09-27 21:58:50 +0300

    base version

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nikolay2019-git/bank
+
+go 1.17
---
+++ pkg/types/types.go
@@ -0,0 +1,43 @@
+package types
+
+//Money представляет собой денежную сумму в минимальных единицах (центы, копейки, дирамы и т.д.)
+type Money int64
+
+// Currency представляет код валюты
+type Currency string
+
+const (
+	TJS Currency = "TJS"
+	USD Currency = "USD"
+	RUB Currency = "RUB"
+	EUR Currency = "EUR"
+)
+
+//PAN представляет номер карты
+type PAN string
+
+//Card представляет информацию о платёжной карте
+type Card struct {
+	ID         int
+	PAN        PAN
+	Balance    Money
+	MinBalance Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+}
+
+type Category string
+
+type Payment struct {
+	ID       int
+	Amount   Money
+	Category Category
+}
+
+type PaymentSource struct {
+	Type    string //'card'
+	Number  string // номер вида "5058 xxxx xxxx 8888"
+	Balance Money  // Баланс в дирамах
+}
